9db0a0179d0cd89bd8a4325ba2784382c5d63e02
Raj K Singh <raj@novvuminc.com>
2017-07-04 12:43:01 -0700

    updated

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-unresolved, import/extensions */
 import { MaterialIcons } from '@expo/vector-icons';
 import React, { PureComponent, PropTypes } from 'react';
-import getIconType from './helpers';
 const propTypes = {
     name: PropTypes.string.isRequired,
     style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
     size: PropTypes.number,
     color: PropTypes.string,
-    type: PropTypes.string
 };
 const defaultProps = { };
 const contextTypes = {
@@ -18,16 +16,10 @@ class Icon extends PureComponent {
     render() {
         const { name, style, size, color, type } = this.props;
         const { palette, spacing } = this.context.uiTheme;
-        let Icon;
-        if (!type) {
-          Icon = getIconType('material');
-        } else {
-          Icon = getIconType(type);
-        }
         const iconColor = color || palette.secondaryTextColor;
         const iconSize = size || spacing.iconSize;
         return (
-            <Icon
+            <MaterialIcons
                 name={name}
                 size={iconSize}
                 color={iconColor}
